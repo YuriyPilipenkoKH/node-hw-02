6dd00ea0b1c062133ead4a4d2dd43f3f6bbca33e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-07-05 13:05:06 +0300

    midleware

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express')
-const books = require('.')
+const books = require('./db/books.json')
+const contacts = require('./db/contacts.json')
+const products = require('./db/products.json')
+const moment = require('moment')
+const fs = require('fs/promises')
+const cors = require('cors')
 
 const app = express()
+
+app.use( async(req, res, next) => {
+    console.log('midleware')
+    const {method, url} = req
+    const date = moment().format('DD-MM-YYYY_hh:mm:ss')
+    await fs.appendFile('./public/server.log',  `\n${method} ${url} ${date}`)
+    next()
+})
+
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+app.get('/books', (req, res) => {
+    res.json(books)
+    // res.send(books)
+})
+app.get('/contacts', (req, res) => {
+    res.json(contacts)
+    // res.send(books)
+})
+app.get('/products', (req, res) => {
+    res.json(products)
+    // res.send(books)
+})
+
+app.use((req , res) => {
+    res.status(404).json({
+        message: 'Not Found'
+    })
+}) 
 
 
 // app.get('/', (req, res) => {
